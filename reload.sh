c970b64e6420048dd6b3b1ca29b0949a1562bc39
alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
2024-11-26 16:52:57 +0100

    Add auto-reload
    Signed-off-by: alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>

---
+++ reload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+user="iut-beziers"
+repo="navi-cheatsheet-alexis-opolka"
+localcheat="$(navi info cheats-path)/${user}__${repo}"
+
+echo "reloading the current cheat sheet..."
+
+if [ -d $localcheat ]; then
+	echo "Cleaning old repo..."
+	rm -fr $localcheat;
+fi
+
+git clone "git@github.com:${user}/${repo}.git" 
